f32d7afac23a864826c6430b33ef49b66e7d87c8
Loe <95266635+loeeeee@users.noreply.github.com>
2025-09-16 00:38:17 +0000

    remove nixos configuration for privacy reason

--- exploration/nixos/mongodb.js
+++
@@ -1,7 +0,0 @@
-// /etc/nixos/initialize-mongo.js
-db = db.getSiblingDB('admin');
-db.createUser({
-  user: 'admin',
-  pwd: 'A_SEHR_SAFE_PASSWORD', // Replace with a strong, unique password
-  roles: [{ role: 'root', db: 'admin' }],
-});
